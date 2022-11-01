f3fa866cb0b9766e8d762f2ec3136db94abb8bf0
Robertspoon <robertthemcjug@gmail.com>
2022-11-01 09:17:18 -0300

    dqwdqwdqwd

--- Loops2/Program.cs
+++ Loops2/Program.cs
@@ -9,27 +9,72 @@ namespace Loops2
     internal class Program
     {
 
-          static int x;
-          static int y;
+        static int x;
+        static int y;
+        
+        static bool gameOver;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Loops");
+          
+
             x = 10;
-            y = -10;
+            y = 10;
+           
+            gameOver = false;
+
+            while(gameOver == false)
+            {
+                PlayerUpdate();
+                DrawPlayer();
+              
+            }
 
 
-            Console.ReadKey(true);
+
         }
         static void PlayerUpdate()
         {
+           ConsoleKeyInfo input;
+           input = Console.ReadKey(true);
+
+            //Console.WriteLine(input);
+            Console.WriteLine(input.KeyChar);
+
+            if (input.KeyChar == 'w')
+            {
+                y--;
+            }
+            else if (input.KeyChar == 's')
+            {
+                y++;
+            }
+            else if (input.KeyChar == 'a')
+            {
+                x--;
+            }
+            else
+            {
+                x++;
+            }
+            if (input.Key == ConsoleKey.Escape)
+            {
+                gameOver = true;
+            }
+            
+
+            
 
            
         }
         static void DrawPlayer()
         {
+
             Console.Clear();
-            Console.SetCursorPosition(x,y);
+            Console.SetCursorPosition(x, y);
             Console.Write(">");
+            
         }
     }
 }
